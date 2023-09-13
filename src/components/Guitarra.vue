a6183926d67a9f20737cae8c628c53c4a15208ba
yonatanlla <yonatanllanto174@gmail.com>
2023-09-13 00:18:33 -0500

    agregar a corrito empezando

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import {ref} from "vue"
+
+    const numero = ref(0)
+    const props = defineProps({
+        guitarra: {
+            type: Object,
+            required: true
+        }
+    })
+
+</script>
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div class="col-4">
+                    <img 
+                        class="img-fluid" 
+                        :src="'/img/' + guitarra.imagen + '.jpg'" 
+                        alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+                    <p>{{ numero }}</p>
+                    <p>{{ guitarra.descripcion }}</p>
+                    <p class="fw-black text-primary fs-3">{{ guitarra.precio }}</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100"
+                        v-on:click="numero++"
+                    >Agregar al Carrito</button>
+                </div>
+            </div><!-- FIN GUITARRA -->
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
